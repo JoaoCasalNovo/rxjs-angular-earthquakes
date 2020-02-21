d0b1d0f4d759b9d64ed801dc89d5fb6e9d5f4d85
João Luis Ribeiro <joao.ribeiro@kelvininc.com>
2020-02-21 17:30:25 +0000

    feat(map): add empty component

---
+++ src/app/earthquake-map/earthquake-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'earthquake-map',
+	templateUrl: './earthquake-map.component.html',
+	styleUrls: ['./earthquake-map.component.less']
+})
+export class EarthquakeMapComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
---
+++ src/app/earthquake-map/earthquake-map.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EarthquakeMapComponent } from './earthquake-map.component';
+
+@NgModule({
+	imports: [
+		CommonModule
+	],
+	declarations: [EarthquakeMapComponent]
+})
+export class EarthquakeMapModule { }
