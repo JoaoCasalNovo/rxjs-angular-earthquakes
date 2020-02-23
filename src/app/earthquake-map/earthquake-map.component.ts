f089890136a269e842ed2543f008cd5e093810ec
miguel.pinto <miguel.pinto@kelvininc.com>
2020-02-23 21:36:57 +0000

    feat(mousemove-popup): add mousemove stream to open a popup

--- src/app/earthquake-map/earthquake-map.component.ts
+++ src/app/earthquake-map/earthquake-map.component.ts
@@ -1,8 +1,10 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { CONTINENTS_CENTER_POINT, DEFAULT_CONTINENT, Quake, PERIODS, DEFAULT_PERIOD } from '../config/map.config';
+import { CONTINENTS_CENTER_POINT, DEFAULT_CONTINENT, Quake, PERIODS, DEFAULT_PERIOD, GeoPoint } from '../config/map.config';
 import * as L from 'leaflet';
 import { QuakeService } from '../services/quake.service';
+import { fromEvent, Observable } from 'rxjs';
+import { map, flatMap, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
 	selector: 'earthquake-map',
@@ -16,14 +18,18 @@ export class EarthquakeMapComponent implements AfterViewInit {
 	map: L.Map;
 	layerGroup;
 
+	quakes: Quake[];
+
+	mousemove$: Observable<GeoPoint>;
+	popup$;
+
 	constructor(private route: ActivatedRoute, private quakeService: QuakeService) { }
 
 	ngAfterViewInit(): void {
 		this.initMap();
 		this.subscribeContinentChanges();
 		this.subscribeQuakeService();
-
-		this.quakeService.load();
+		this.subscribePopup();
 	}
 
 	initMap() {
@@ -34,7 +40,6 @@ export class EarthquakeMapComponent implements AfterViewInit {
 
 		this.layerGroup = L.layerGroup();
 		this.layerGroup.addTo(this.map);
-		this.layerGroup.on('click', this.onCircleClick);
 	}
 
 	// EXERCICIO
@@ -50,26 +55,47 @@ export class EarthquakeMapComponent implements AfterViewInit {
 	// EXERCICIO
 	subscribeQuakeService() {
 		this.quakeService.quakes
+			.pipe(distinctUntilChanged())
 			.subscribe((quakes: Quake[]) => {
-				console.log(quakes);
-
+				this.quakes = quakes;
+				this.layerGroup.clearLayers();
 				quakes.forEach(quake => this.addCircle(quake));
 			});
+
+		this.quakeService.load();
+	}
+
+	// EXERCICIO
+	subscribePopup() {
+		this.mousemove$ = fromEvent(this.map, 'mousemove')
+			.pipe(map((mouseMove: any) => ({
+				lat: mouseMove.latlng.lat,
+				lng: mouseMove.latlng.lng
+			} as GeoPoint)));
+
+		this.mousemove$
+			.pipe(
+				flatMap((point: GeoPoint) => {
+					return (this.quakes && this.quakes.filter(quake => {
+						return quake.lat.toFixed(1) === point.lat.toFixed(1) && quake.lng.toFixed(1) === point.lng.toFixed(1);
+					}));
+				})
+			).subscribe(quake => this.openPopup(quake));
 	}
 
 	addCircle(quake: Quake) {
-		const circle = L.circle([quake.lat, quake.lng], quake.size);
-		circle.on('click', this.onCircleClick);
-		circle.addTo(this.layerGroup);
+		L.circle([quake.lat, quake.lng], quake.size).addTo(this.layerGroup);
 	}
 
 	onPeriodChanged(period: string) {
-		this.layerGroup.clearLayers();
 		this.selectedPeriod = period;
 		this.quakeService.load(period);
 	}
 
-	onCircleClick(ev) {
-		console.log('asdasdasdas', ev);
+	openPopup(quake: Quake) {
+		L.popup()
+			.setLatLng({ lat: quake.lat, lng: quake.lng })
+			.setContent(`<p><h4>${quake.place}</h4><ul><li>${quake.size / 20000}</li><li>${quake.type}</li><li>${quake.time}</li></ul></p>`)
+			.openOn(this.map);
 	}
 }
